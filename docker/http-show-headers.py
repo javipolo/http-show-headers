f0a8917e3ed452c57bb3845b77a0ac5f26a5d741
Javi Polo <javi@kodify.io>
2019-05-20 16:28:27 +0200

    Add debug

--- docker/http-show-headers.py
+++ docker/http-show-headers.py
@@ -7,17 +7,20 @@ port = 8080
 from pprint import pformat
 from bottle import route, run, request, static_file
 from json import dumps
+from os import environ
 
 @route('/healthz')
 def get_health():
     return "true"
-
 
 @route('/:#.*#', method='ANY')
 def print_headers():
     my_dict = {}
     for k in request.headers:
         my_dict[k] = request.headers[k]
+    if 'DEBUG' in environ:
+        if environ['DEBUG'] == 'true':
+            print dumps(my_dict, sort_keys=True, indent=2)
     return dumps(my_dict, sort_keys=True, indent=2)
 
 run(host=address, port=port)
